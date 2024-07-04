efe06d1dd1bb4c45b7d640de0d242075dbf6da18
Lukas Angerer <executor.dev@gmail.com>
2024-07-04 12:02:37 +0200

    Fresh project with dotnet new maui - 8.0.40

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace FlexLayoutAlignContent;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace FlexLayoutAlignContent;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
